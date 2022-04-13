1fa9cdd2310182bb4a8bf10c392a710ce149b720
BGM-109 <vividxxxxx@gmail.com>
2022-04-13 11:06:36 +0900

    first commit

---
+++ components/banner.js
@@ -0,0 +1,9 @@
+const Banner = () => {
+  return (
+    <div className="w-full h-80 bg-black text-white flex items-center justify-center">
+      <h2>Get your Sneakers</h2>
+    </div>
+  );
+};
+
+export default Banner;
